8a4bc787a7c97f66884e599bb88223652e8fd3f1
Leith <Leith>
2017-08-04 20:48:13 +0200

    Added disappearing platforms.

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Platform : NetworkBehaviour {
+
+	[SyncVar]
+	public float timer;
+	public bool permanent;
+
+	void Update () {
+		if (permanent) {
+			return;
+		}
+
+		if (isServer) {
+			timer -= Time.deltaTime;
+		}
+
+		if (timer <= 0) {
+			Activate ();
+		}
+		else if (timer <= 3){
+			Warn();
+		}
+	}
+
+	void Activate(){
+		NetworkServer.Destroy (gameObject);
+	}
+
+	void Warn(){
+		GetComponent<MeshRenderer> ().material.color = new Color (1.0f, 0.0f, 0.0f, 0.5f);
+		/*
+		print ("warning");
+		Component[] renderers = GetComponents<Renderer> ();
+		foreach (Renderer r in renderers) {
+			Color color;
+			foreach (Material m in r.materials) {
+				color = m.color;
+				color.a = 0.2f;
+				m.color = color;
+			}
+		}*/
+		//Color color = renderer.material.color;
+		//color.a = 0.5f;
+		//GetComponent<MeshRenderer> ().material.color = color;
+	}
+}
